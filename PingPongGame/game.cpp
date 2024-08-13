5acef3e1d86de7148c821a419e4264dead975a44
Parsifal22 <73550345+Parsifal22@users.noreply.github.com>
2024-08-13 22:13:28 +0300

    Improved the physics of player movement, and also added a 2nd player

--- PingPongGame/game.cpp
+++ PingPongGame/game.cpp
@@ -4,18 +4,31 @@
 #define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)
 
-float player_pos_x = 0.f;
-float player_pos_y = 0.f;
+float player_1_p, player_1_dp, player_2_p, player_2_dp;
 
 void simulate_game(Input* input, float dt) {
 	clear_screen(0xff5500);
-	float speed = 50.f; // Units per second
-	if (is_down(BUTTON_UP)) player_pos_y += speed * dt;
-	if (is_down(BUTTON_DOWN)) player_pos_y -= speed * dt;
-	if (is_down(BUTTON_RIGHT)) player_pos_x += speed * dt;
-	if (is_down(BUTTON_LEFT)) player_pos_x -= speed * dt;
-	
-	draw_rect(player_pos_x, player_pos_y, 1, 1, 0xff0000);
-	draw_rect(30, 30, 5, 5, 0xff0000);
-	draw_rect(-20, 20, 8, 3, 0xff0000);
+	draw_rect(0, 0, 85, 45, 0xffaa33);
+
+	float player_1_ddp = 0.f;
+	if (is_down(BUTTON_UP)) player_1_ddp += 2000;
+	if (is_down(BUTTON_DOWN)) player_1_ddp -= 2000;
+
+	player_1_ddp -= player_1_dp * 10.f;
+
+	player_1_p = player_1_p + player_1_dp * dt + player_1_ddp * dt * dt * .5f;
+	player_1_dp = player_1_dp + player_1_ddp * dt;
+
+	float player_2_ddp = 0.f;
+	if (is_down(BUTTON_W)) player_2_ddp += 2000;
+	if (is_down(BUTTON_S)) player_2_ddp -= 2000;
+
+	player_2_ddp -= player_2_dp * 10.f;
+
+	player_2_p = player_2_p + player_2_dp * dt + player_2_ddp * dt * dt * .5f;
+	player_2_dp = player_2_dp + player_2_ddp * dt;
+
+	draw_rect(0, 0, 1, 1, 0xffffff);
+	draw_rect(80, player_1_p, 2.5, 12, 0xff0000);
+	draw_rect(-80, player_2_p, 2.5, 12, 0xff0000);
 }
